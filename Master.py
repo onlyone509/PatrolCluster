ef063531c8d6611ef08fbca684c63ec0790875a5
onlyone509 <onlyone509@163.com>
2015-08-15 20:41:47 +0800

    1. 新增作业队列功能 2. 更新部分函数的签名 3. 新增配置文件功能

--- Master.py
+++ Master.py
@@ -1,56 +1,112 @@
 #!python
 #coding=utf8
 from SimpleXMLRPCServer import SimpleXMLRPCServer
+import ConfigParser
 import xmlrpclib
 import string
 import thread
 import time
+import Queue
+from ClusterJob import ClusterJob
+from DataDic import dic_slave_status
+import pdb
 
 class Master:
-    def __init__(self):
-        self.port = 8859
+    def __init__(self, port, job_queue_size, heart_beat_interval):
+        self.port = port
         self.status = 0
         self.server = SimpleXMLRPCServer(("localhost", self.port))
         self.server.register_multicall_functions()
-        self.server.register_function(self.add, 'add')
         self.server.register_function(self.register, 'register')
         self.server.register_function(self.heart_beat, 'heart_beat')
-        self.slave_list = []
-    
-    # 测试函数
-    def add(self, x, y):
-        print "X : " + str(x) + " ; Y : " + str(y)
-        return x + y
+        self.server.register_function(self.update_status, 'update_status')
+        self.heart_beat_interval = heart_beat_interval
+        self.slave_list = {}
+        self.to_do_job_queue = Queue.Queue(job_queue_size)
+        self.doing_job_list = []
     
     # 心跳报文
     def heart_beat(self, ip, status):
         #print "hello world"
-        print "HeartBeat: " + str(ip) + " is " + str(status)
+        print "HeartBeat: " + str(ip) + " is " + dic_slave_status[status]
+        self.slave_list[ip][status] = status
+        return 0
+    
+    # 更新slave状态
+    def update_status(self, ip, status):
+        print "update status: " + str(ip) + " is " + dic_slave_status[status]
+        self.slave_list[ip][status] = status
         return 0
     
     # 注册新slave节点
-    def register(self, url):
+    def register(self, ip, port, status):
+        url = "http://%s:%i/" % (ip, port)
         proxy = xmlrpclib.ServerProxy(url)
-        self.slave_list.append(proxy)
-        return len(self.slave_list)
+        self.slave_list[ip] = {"proxy":proxy, "status":status}
+        return (len(self.slave_list), self.heart_beat_interval)
     
     # 启动master服务
     def run(self):
         print "Master is running, listening port 8859..."
-        thread.start_new_thread(self.check_task_list, ())
+        thread.start_new_thread(self.check_job_list, ())
         self.server.serve_forever()
+    
+    # 停止master服务
+    def stop(self):
+        self.server.server_close()
+        self.status = 99
+        return 0
+    
+    def add_job(self, job):
+        if not self.to_do_job_queue.full():
+            # to-do: 写入数据库，持久存储
+            create_time = time.time()
+            job.create_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(create_time))
+            job.job_id = int(create_time)
+            self.to_do_job_queue.put(job)
+            return job.job_id
+        else:
+            return -1
     
     # 检查任务队列
     # todo：如队列不空，取出任务执行
-    def check_task_list(self):
+    def check_job_list(self): 
+        print "check_job_list is called: " + str(self.status)
         while self.status != 99:
-            if len(self.slave_list) > 0:
-                task_id = self.slave_list[0].work("test_test_test")
-                print "task_id is " + str(task_id)
+            if not self.to_do_job_queue.empty():    # 如果待执行队列不为空
+                print "queue is not empty"
+                # 找到一个空闲的slave
+                idle_slave = None
+                for ip in self.slave_list.keys():
+                    if self.slave_list[ip]["status"] == 0:
+                        idle_slave = self.slave_list[ip]["proxy"]
+                        break
+                else:
+                    continue
+                print "working"
+                # 从队列中取出一个作业，由空闲的slave运行
+                job = self.to_do_job_queue.get()
+                status = idle_slave.work(job.job_id, job.job_cmd, job.job_para_list)
+                if status != -1:
+                    # to-do: 更新数据库
+                    job.job_status = 1
+                    self.doing_job_list.append(job)
+                    print "job_" + str(job.job_id) + "is running"
+            else:
+                print "queue is empty"
                 time.sleep(15)
 
 def main():
-    master = Master()
+    config = ConfigParser.ConfigParser()
+    config.read("conf.ini")
+    port = int(config.get("master configuration", "listening port"))
+    job_queue_size = int(config.get("master configuration", "job queue size"))
+    heart_beat_interval = int(config.get("master configuration", "heart beat interval"))
+    master = Master(port, job_queue_size, heart_beat_interval)
+    
+    # 测试用
+    job = ClusterJob('test', "test1", [])
+    master.add_job(job)
     master.run()
     
 main()
--- Slave.py
+++ Slave.py
@@ -4,15 +4,17 @@ from SimpleXMLRPCServer import SimpleXMLRPCServer
 import xmlrpclib
 import thread
 import time
+import ConfigParser
 import pdb
 
 class Slave:
-    def __init__(self):
-        self.port   = 8860
-        self.status = 0
-        self.master_url = "http://localhost:8859/"
-        self.heart_beat_interval = 5
-        self.server = SimpleXMLRPCServer(("localhost", self.port))
+    def __init__(self, ip, port, master_url):
+        self.port   = port
+        self.ip     = ip
+        self.status = 0     #0是空闲，1是忙碌，2是异常，99是关闭
+        self.master_url = master_url
+        self.heart_beat_interval = 0
+        self.server = SimpleXMLRPCServer((ip, self.port))
         self.server.register_multicall_functions()
         self.server.register_function(self.work, "work")
         self.server.register_function(self.stop, "stop")
@@ -23,7 +25,7 @@ class Slave:
         
     def run(self):
         #pdb.set_trace()
-        my_worker_id = self.master_proxy.register("http://localhost:8860/")
+        (my_worker_id, self.heart_beat_interval) = self.master_proxy.register(self.ip, self.port, self.status)
         print "My work_id is " + str(my_worker_id)
         thread.start_new_thread(self.send_heart_beat, ())
         return self.server.serve_forever()
@@ -33,29 +35,41 @@ class Slave:
         self.status = 99
         return 0
         
-    def work(self, parameter):
-        task_id = 99999
-        print "run task_" + str(task_id) + ":" + parameter
-        thread.start_new_thread(self.run_a_task, (task_id, parameter))
-        return task_id
+    def work(self, job_id, job_cmd, para_list):
+        print "run job_" + str(job_id) + ":" + str(para_list)
+        try:
+            thread.start_new_thread(self.run_a_job, (job_id, job_cmd, para_list))
+            self.status = 1
+            self.master_proxy.update_status(self.ip, self.status)
+        except:
+            return -1
+        return job_id
     
-    def run_a_task(self, task_id, parameter):
-        print "start, task_" + str(task_id) + ":" + parameter
-        time.sleep(10)
-        print "finish, task_" + str(task_id) + ":" + parameter
+    def run_a_job(self, job_id, job_cmd, parameter):
+        print "start, " + str(job_cmd) + ":" + str(parameter)
+        
+        time.sleep(30)
+        self.status = 0
+        print "finish, " + str(job_cmd) + ":" + str(parameter)
     
     def send_heart_beat(self):
         while self.status != 99:
-            self.master_proxy.heart_beat('192.168.1.1', 'busy')
+            self.master_proxy.heart_beat(self.ip, self.status)
             #print "send heart_beat"
             time.sleep(self.heart_beat_interval)
-        
-    def add(self, x, y):
-        print "X : " + str(x) + " ; Y : " + str(y)
-        return x + y
+        else:
+            self.master_proxy.heart_beat(self.ip, self.status)
+            #print "send heart_beat"
+            time.sleep(self.heart_beat_interval)
 
 def Main():
-    slave = Slave()    
+    config = ConfigParser.ConfigParser()
+    config.read("conf.ini")
+    ip = config.get("slave configuration", "ip address")
+    port = int(config.get("slave configuration", "listening port"))
+    master_url = config.get("slave configuration", "master url")
+    print master_url
+    slave = Slave(ip, port, master_url)
     slave.run()
 
 Main()
